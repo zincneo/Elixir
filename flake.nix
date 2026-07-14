fc147641fc4a0de37dc49ebc323651e034056688
zincneo <zincneoestonia@gmail.com>
2026-07-14 17:28:37 +0800

    Init: 配置初始化Elixir开发环境的flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Elixir development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.beamMinimalPackages.elixir
+              pkgs.beamMinimalPackages.erlang
+              pkgs.beamMinimalPackages.rebar3
+              pkgs.gitMinimal
+            ];
+          };
+        }
+      );
+    };
+}
